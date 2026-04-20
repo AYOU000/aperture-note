3eb7de703cdf6a637372bdb3107366c94ca0d6da
ayoub <benayoub0908@gmail.com>
2026-04-20 21:49:14 +0800

    complete auth flow and optimize dockerized environment

--- apps/server/src/middleware/validate.ts
+++ apps/server/src/middleware/validate.ts
@@ -4,7 +4,7 @@ import { ZodObject } from "zod";
 export const validate = (schema: ZodObject ) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const result = await schema.safeParseAsync(req.body);
-
+    
     if (!result.success) {
       return res.status(400).json({
         success: false,
